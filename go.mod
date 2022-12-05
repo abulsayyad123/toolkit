3cafd40d43608808dd2aac062724a772234579d2
AbulAsar S <abul.asar22@gmail.com>
2022-12-05 14:11:02 +0530

    initial commit with randomstring functionality

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/abulsayyad123/toolkit
+
+go 1.19
---
+++ tools.go
@@ -0,0 +1,19 @@
+package toolkit
+
+import "crypto/rand"
+
+const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+
+// Tools is the type used to instantiate this module. Any variable of this type will have access to all the methods with the receiver poionter to Tools.
+type Tools struct{}
+
+// RandomString returns a string of Random characters of length n, using using randomStringSource as a source for string
+func (t *Tools) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomStringSource)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+	return string(s)
+}
